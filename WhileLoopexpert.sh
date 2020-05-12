128fb43943bca21286bb7d9213fb70552682c01b
mohsenrp <mohsenrp@outlook.com>
2020-05-12 14:29:45 +0430

    990223

---
+++ WhileLoopexpert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# 5/2/2017
+#
+# script5 - Linux Scripting Book
+tput clear # clear the screen
+row=1
+while [ $row -le $LINES ]; do
+    col=1
+    while [ $col -le $COLUMNS ]; do
+        echo -n "#"
+        let col++
+    done
+    echo "" # output a carriage return
+    let row++
+done
+exit 0
